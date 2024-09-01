00d3267e5c7ff4970f068479cb2b261674d097fe
Abhi <abhidaa1233@gmail.com>
2024-09-01 18:57:49 +0530

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,18 @@
+// Add custom JavaScript here
+function runTypingEffect() {
+  const text = 'I am Abhi...';
+  const typingElement = document.getElementById('typing-text');
+  const typingDelay = 200;
+
+  typeText(text, typingElement, typingDelay);
+}
+
+function typeText(text, typingElement, delay) {
+  for (let i = 0; i < text.length; i++) {
+    setTimeout(() => {
+      typingElement.textContent += text.charAt(i);
+    }, delay * i);
+  }
+}
+
+document.addEventListener('DOMContentLoaded', runTypingEffect);
